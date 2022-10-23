b892a5db5aadefe805aaa98249fcbb8568d27c2f
mmi <efficiel.technologie@gmail.com>
2022-10-23 23:46:50 +0200

    2346

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
 export function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Turpis cursus in hac
+      habitasse platea dictumst. Lorem ipsum dolor sit amet consectetur
+      adipiscing elit pellentesque habitant. Non consectetur a erat nam at
+      lectus. Sed odio morbi quis commodo odio aenean sed. Ut morbi tincidunt
+      augue interdum velit euismod in pellentesque massa. Nisi lacus sed viverra
+      tellus in hac habitasse platea. Eget est lorem ipsum dolor sit amet
+      consectetur. Ligula ullamcorper malesuada proin libero nunc consequat.
+      Facilisi etiam dignissim diam quis. Iaculis at erat pellentesque
+      adipiscing commodo. Nibh sit amet commodo nulla facilisi nullam vehicula.
+      In nulla posuere sollicitudin aliquam ultrices sagittis orci.
+    </div>
+  );
 }
